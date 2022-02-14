7374c404404cd344dadf35a7c7c93bfddd7282d0
donr82 <donr82@sbcglobal.net>
2022-02-14 16:48:29 -0600

    initial commit and solution

---
+++ Multiples3And5/Multiples3And5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 1000).Where(n => n % 3 == 0 || n % 5 == 0);
+            
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
